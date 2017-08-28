43ed444b58f6b967ac1f67c5579044acf055e37f
kang <kjxstar@gmail.com>
2017-08-28 18:21:50 +0800

    string count

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/StringExercises.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/StringExercises.java
@@ -2,6 +2,8 @@ package com.insightfullogic.java8.exercises.chapter3;
 
 import com.insightfullogic.java8.exercises.Exercises;
 
+import java.nio.CharBuffer;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -10,12 +12,12 @@ public class StringExercises {
 
     // Question 7
     public static int countLowercaseLetters(String string) {
-        return Exercises.replaceThisWithSolution();
+        return (int) CharBuffer.wrap(string).chars().filter(value -> value >= 'a' && value <= 'z').count();
     }
 
     // Question 8
     public static Optional<String> mostLowercaseString(List<String> strings) {
-        return Exercises.replaceThisWithSolution();
+        return strings.stream().max(Comparator.comparingInt(StringExercises::countLowercaseLetters));
     }
 
 }

46df899dddc92616a6276c4f8366099c14086742
kang <kjxstar@gmail.com>
2017-08-28 18:01:48 +0800

    yes we can

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/FilterUsingReduce.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/FilterUsingReduce.java
@@ -3,6 +3,7 @@ package com.insightfullogic.java8.exercises.chapter3;
 import com.insightfullogic.java8.exercises.Exercises;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -13,7 +14,19 @@ import java.util.stream.Stream;
 public class FilterUsingReduce {
 
     public static <I> List<I> filter(Stream<I> stream, Predicate<I> predicate) {
-        return Exercises.replaceThisWithSolution();
+        return stream.reduce(Collections.<I>emptyList(), (List<I> is, I i) -> {
+            if (predicate.test(i)) {
+                List<I> l = new ArrayList<>(is);
+                l.add(i);
+                return l;
+            } else {
+                return is;
+            }
+        }, (is, is2) -> {
+            List<I> newLeft = new ArrayList<>(is);
+            newLeft.addAll(is2);
+            return newLeft;
+        });
     }
 
 }

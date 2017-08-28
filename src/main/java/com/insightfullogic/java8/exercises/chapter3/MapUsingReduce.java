b5386ba7832db9e68b4f36b3525e45621c0ebe57
kang <kjxstar@gmail.com>
2017-08-28 18:11:27 +0800

    yes we can * 2

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/MapUsingReduce.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/MapUsingReduce.java
@@ -3,6 +3,7 @@ package com.insightfullogic.java8.exercises.chapter3;
 import com.insightfullogic.java8.exercises.Exercises;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -13,7 +14,17 @@ import java.util.stream.Stream;
 public class MapUsingReduce {
 
     public static <I, O> List<O> map(Stream<I> stream, Function<I, O> mapper) {
-        return Exercises.replaceThisWithSolution();
+        return stream.reduce(Collections.emptyList(), (os, i) -> {
+                    List<O> o = new ArrayList<>(os);
+                    o.add(mapper.apply(i));
+                    return o;
+                },
+                (os, os2) -> {
+                    List<O> o = new ArrayList<>(os);
+                    o.addAll(os2);
+                    return o;
+                }
+        );
     }
 
 }
